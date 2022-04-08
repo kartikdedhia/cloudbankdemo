a8eb518a35a7d53964396fc69f6cf35fb8172b29
anderson fonseca <anderson.fonseka@gmail.com>
2022-04-08 15:07:52 -0300

    fv

--- src/main/java/br/com/peers/cloudbankdemo/bean/Cliente.java
+++ src/main/java/br/com/peers/cloudbankdemo/bean/Cliente.java
@@ -14,6 +14,10 @@ public class Cliente {
 		super();
 		this.nome = nome;
 	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getId() {
 		return id;
--- src/main/java/br/com/peers/cloudbankdemo/service/ClienteService.java
+++ src/main/java/br/com/peers/cloudbankdemo/service/ClienteService.java
@@ -1,7 +1,12 @@
 package br.com.peers.cloudbankdemo.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,21 +18,27 @@ import br.com.peers.cloudbankdemo.bean.Cliente;
 @RestController
 public class ClienteService {
 
-	private List<Cliente> clientes = new ArrayList<Cliente>();
+	private Map<String, Cliente> clientes = new HashMap<String, Cliente>();
 	
 	public ClienteService() {
-		clientes.add(new Cliente("Anderson Fonseca"));
-		
+		Cliente c = new Cliente("Anderson Fonseca");
+		clientes.put(c.getId(), c);
 	}
 	
 	@GetMapping("/clientes")
 	public List<Cliente> list() {	
-		return this.clientes;
+		return new ArrayList<Cliente>(this.clientes.values());
+	}
+	
+	@GetMapping("/clientes/{id}")
+	public Cliente get(@PathParam("id") String id) {	
+		return this.clientes.get(id);
 	}
 	
 	@PostMapping("/clientes")
 	public Cliente add(@RequestBody Cliente cliente) {
-		this.clientes.add(cliente);
+		cliente.setId(UUID.randomUUID().toString());
+		clientes.put(cliente.getId(), cliente);
 		return cliente;
 	}
 	
