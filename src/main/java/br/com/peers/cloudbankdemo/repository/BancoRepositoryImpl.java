69c6343ffa7615a69fc9e328d24c25caa84f8d9d
AndersonFonsecaeSilv <AndersonFonsecaeSilv@PEERS7RSBKH3>
2022-04-14 11:03:18 -0300

    Banco Service

---
+++ src/main/java/br/com/peers/cloudbankdemo/repository/BancoRepositoryImpl.java
@@ -0,0 +1,48 @@
+package br.com.peers.cloudbankdemo.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.peers.cloudbankdemo.bean.Banco;
+
+@Repository
+public class BancoRepositoryImpl implements BancoRepository {
+	
+	private Map<String, Banco> bancos = new HashMap<String, Banco>();
+
+	public List<Banco> list() {	
+		return new ArrayList<Banco>(this.bancos.values());
+	}
+	
+	public Banco get(String id) {	
+		return this.bancos.get(id);
+	}
+	
+	public Banco add(Banco banco) {
+		banco.setId(UUID.randomUUID().toString());
+		bancos.put(banco.getId(), banco);
+		return banco;
+	}
+	
+	public Banco edit(Banco banco) {
+		
+		if (bancos.containsKey(banco.getId())) {
+			bancos.put(banco.getId(), banco);
+		}
+
+		return banco;
+	}
+	
+	public void remove(String id) {
+		
+		if (bancos.containsKey(id)) {
+			bancos.remove(id);
+		}
+	}
+
+}
---
+++ src/main/java/br/com/peers/cloudbankdemo/service/BancoService.java
@@ -0,0 +1,53 @@
+package br.com.peers.cloudbankdemo.service;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.peers.cloudbankdemo.bean.Banco;
+import br.com.peers.cloudbankdemo.repository.BancoRepository;
+
+@RestController
+@RequestMapping("/bancos")
+public class BancoService {
+
+	@Autowired
+	private BancoRepository bancoRepository;
+	
+	public BancoService() {}
+	
+	@GetMapping
+	public List<Banco> list() {	
+		return bancoRepository.list();
+	}
+	
+	@GetMapping("/{id}")
+	public Banco get(@PathParam("id") String id) {	
+		return bancoRepository.get(id);
+	}
+	
+	@PostMapping
+	public Banco add(@RequestBody Banco cliente) {
+		return bancoRepository.add(cliente);
+	}
+	
+	@PutMapping
+	public Banco edit(@RequestBody Banco cliente) {
+		return bancoRepository.edit(cliente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void remove(@PathParam("id") String id) {
+		bancoRepository.remove(id);
+	}
+	
+}
