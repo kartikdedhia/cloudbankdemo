b85c0e81c6de894eb8c6242c7c317e3addca9af1
AndersonFonsecaeSilv <AndersonFonsecaeSilv@PEERS7RSBKH3>
2022-04-14 17:24:44 -0300

    Update All

--- src/main/java/br/com/peers/cloudbankdemo/Application.java
+++ src/main/java/br/com/peers/cloudbankdemo/Application.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Bank API", version = "2.0", description = "Bank Information"))
+@OpenAPIDefinition(info = @Info(title = "CloudBankDemo API", version = "2.0", description = "Bank Information"))
 public class Application {
 
 	public static void main(String[] args) {
